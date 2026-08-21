17310c5f37c08c903898e77743dca2ad733d8311
D_Christian <159540251+Christian052@users.noreply.github.com>
2026-08-21 23:03:37 +0200

    update .env

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "/api",
+  baseURL: import.meta.env.VITE_API_URL ||"https://a-e-tech-project.onrender.com/" || "/api",
   withCredentials: true, // Send httpOnly refresh cookie
 });
 

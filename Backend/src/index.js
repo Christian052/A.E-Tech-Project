01111e9f400034b07f24e6d1a771775a6a8984fe
D_Christian <159540251+Christian052@users.noreply.github.com>
2026-08-21 22:39:50 +0200

    update .env

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -45,7 +45,7 @@ app.use(
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+    origin: process.env.CORS_ORIGIN || "https://augusmart.vercel.app/",
     credentials: true,
   })
 );
